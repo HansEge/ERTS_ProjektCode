44420cd299ff71914e06e2afb3ba1d9d8266bbb7
Stinus <stinus2@gmail.com>
2019-12-19 13:31:46 +0100

    tilføjer de forskellige states

---
+++ ROGSAnne_simple/ROGSAnne_simple.sdk/ROGSAnne_simple/Evaluator.cpp
@@ -0,0 +1,18 @@
+/*
+ * Evaluator.cpp
+ *
+ *  Created on: 19 Dec 2019
+ *      Author: stinu
+ */
+
+#include "Evaluator.h"
+
+Evaluator::Evaluator() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Evaluator::~Evaluator() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ ROGSAnne_simple/ROGSAnne_simple.sdk/ROGSAnne_simple/Setup.cpp
@@ -0,0 +1,18 @@
+/*
+ * Setup.cpp
+ *
+ *  Created on: 19 Dec 2019
+ *      Author: stinu
+ */
+
+#include "Setup.h"
+
+Setup::Setup() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Setup::~Setup() {
+	// TODO Auto-generated destructor stub
+}
+
