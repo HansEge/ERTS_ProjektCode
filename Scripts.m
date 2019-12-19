c11dde3eafd7e9aceae409c416658559584f5344
Mathias <201505665@post.au.dk>
2019-12-19 09:13:58 +0100

    Updating graph of number of solutions

--- Scripts.m
+++ Scripts.m
@@ -4,11 +4,15 @@ clc; clear; close all;
 
 number_of_points = [1:10];
 number_of_solutions = factorial(number_of_points);
+NoS_string = num2cell(number_of_solutions)
+
 figure(1)
 semilogy(number_of_points, number_of_solutions, '-o');
 xlabel('Number of points');
 ylabel('Number of possible solutions');
+xlim([0.8 12.2])
 % TODO: Add markers for data.
 
 disp(['Number of points','    Number of possible solutions'])
 disp([number_of_points',number_of_solutions'])
+text(number_of_points+0.1,number_of_solutions, NoS_string)
