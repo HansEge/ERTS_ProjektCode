20b8d66d08f3004a82b58fb148899dbf355f8bc2
Stinus <stinus2@gmail.com>
2019-12-19 13:39:03 +0100

    Tilføjer de sidste klasser

---
+++ ROGSAnne_simple/ROGSAnne_simple.sdk/ROGSAnne_simple/UserInterface.h
@@ -0,0 +1,17 @@
+/*
+ * UserInterface.h
+ *
+ *  Created on: 19 Dec 2019
+ *      Author: stinu
+ */
+
+#ifndef USERINTERFACE_H_
+#define USERINTERFACE_H_
+
+class UserInterface {
+public:
+	UserInterface();
+	virtual ~UserInterface();
+};
+
+#endif /* USERINTERFACE_H_ */
