97b0522c30761d8b353ea90be8ba32fe5cd6feb2
Stinus <stinus2@gmail.com>
2019-12-22 11:33:47 +0100

    Lavet GLOBAL.h der indeholder et par defines

---
+++ ROGSAnne_simple/ROGSAnne_simple.sdk/ROGSAnne_simple/GLOBAL.h
@@ -0,0 +1,15 @@
+/*
+ * GLOBAL.h
+ *
+ *  Created on: 22 Dec 2019
+ *      Author: stinu
+ */
+
+#ifndef GLOBAL_H_
+#define GLOBAL_H_
+
+#define NUM_COORDS 10
+#define POPULATION_SIZE 10
+
+
+#endif /* GLOBAL_H_ */
