2c57386394c7cc9cbe2d711798a556092eabe3a5
Stinus <stinus2@gmail.com>
2019-12-28 13:49:01 +0100

    Lol

--- HLS/Distance_test.cpp
+++ HLS/Distance_test.cpp
@@ -8,17 +8,19 @@ int main(int argc, char **argv)
    int x[NUM_COORDS] = {0,1,2,3,4,5,6,7,8,9};
    int y[NUM_COORDS] = {0,1,2,3,4,5,6,7,8,9};
    
-   
    float distance_sw, distance_hw = 0;
-   
-   int err_cnt = 0;
 
-   // Generate the expected result
-   distance_sw = Distance(x,y);
+   float *dist_hw_ptr = &distance_hw;
+   	// Calculate total distance
+	for(int i = 0; i < NUM_COORDS-1; i++)
+	{
+		distance_sw += sqrt(pow(x[i+1]-x[i],2) + pow(y[i+1]-y[i],2));
+	}
+
    
    #ifdef HW_COSIM
    // Run the AutoESL matrix multiply block
-   //distance_hw = DistanceCalculator(x,y);
+	distance_hw = distCalc(x,y);
    #endif
 	
 	//Print sw result
